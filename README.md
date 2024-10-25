4c86a53877d4d67d0dbe57bee40c4e584929474b
friquelmel9 <friquelmel.137@gmail.com>
2024-10-25 17:52:19 -0300

    Cuestionario.py de inf-253: Version 0.1 Version alpha 1 del cuestionario!
    Con respecto al programa:
        -El programa permite hacer un cuestionario con respecto a la unidad 5 del curso
        -Hay preguntas de verdadero-falso tanto como alternativas. Incluye de creacion personal como proveniente de otras fuentes
        -Tras la realizacion de un cuestionario, te entrega tu puntaje correspondiente
    
    Mejoras:
        -Nuevo formato de los txt. Permite separar entre preguntas originales o de previas evaluaciones
        -Nuevas funciones que incluyen mejoras con respecto a la version anterior
        -Cambio de algunas preguntas, eliminacion de preguntas ambiguas, entre otros

--- README.md
+++ README.md
@@ -8,24 +8,22 @@ Si se desea, se pueden ver todas las preguntas en la seccion "Cuestionario"
 
 ## Bibliografia Utilizada
 
-De donde saieron las preguntas:
-    - Compilado quizzes-Marti
-    - Certamen 1-Marti
-    - Control 3-Gabriel Carmona 2022
-    - Quiz 3 CC-Jose Luis Marti 2022
-    - Quiz 3 SJ-Roberto Diaz 2022
-    - Quiz 3 P2 - Jose Luis Marti
+De donde saieron las preguntas:--
+    - Compilado quizzes-Marti--
+    - Certamen 1-Marti--
+    - Control 3-Gabriel Carmona 2022--
+    - Quiz 3 CC-Jose Luis Marti 2022--
+    - Quiz 3 SJ-Roberto Diaz 2022--
+    - Quiz 3 P2 - Jose Luis Marti--
 
 
 Clases:
-    - Presentaciones Jose Marti
-
-JAVA:
-    - Profesor, le agradezco :p
+    - Presentaciones Jose Marti--
+    - Ejemplos de clase 2024 W. Ormazábal--
 
 De donde obtener esta informacion:
-    - [Almacen Informatico](https://onedrive.live.com/?id=4B834E9512402066!164&cid=4B834E9512402066&redeem=aHR0cHM6Ly8xZHJ2Lm1zL2YvYy80YjgzNGU5NTEyNDAyMDY2L0VtWWdRQktWVG9NZ2dFdWtBQUFBQUFBQjAzX3haX2hjNVN5bC04UlJPZm41UEE_ZT1BQ3NTZzM)
-    - Con cuenta usm; [Almacen Camello](https://usmcl-my.sharepoint.com/:f:/g/personal/carlos_bravora_usm_cl/Et9ifP3zE8RMlQtTDj2X2pgB3FFeag78iDEe6ecSs4MJgA?e=xsGAea)
+    - [Almacen Informatico](https://onedrive.live.com/?id=4B834E9512402066!164&cid=4B834E9512402066&redeem=aHR0cHM6Ly8xZHJ2Lm1zL2YvYy80YjgzNGU5NTEyNDAyMDY2L0VtWWdRQktWVG9NZ2dFdWtBQUFBQUFBQjAzX3haX2hjNVN5bC04UlJPZm41UEE_ZT1BQ3NTZzM)--
+    - Con cuenta usm; [Almacen Camello](https://usmcl-my.sharepoint.com/:f:/g/personal/carlos_bravora_usm_cl/Et9ifP3zE8RMlQtTDj2X2pgB3FFeag78iDEe6ecSs4MJgA?e=xsGAea)--
 
 ## Derechos reservados
 
--- cuestionario-inf253.py
+++ cuestionario-inf253.py
@@ -4,187 +4,157 @@ import os
 # Para estos dict, considere key la unidad, mientras que el valor dentro una lista con todo lo necesario
 dict_vf = dict()
 dict_alt = dict()
-dict_figuras = dict() #Borrarlo despues de reinicio
 dict_fig = dict()
 
 # Sub dict con aquellas preguntas que vienen referenciadas de anteriores ejemplos
-dict_vf_evaluacion = dict()
-dict_alt_evaluacion = dict()
+dict_vf_ref = dict()
+dict_alt_ref = dict()
 
 def getFig(unidad):
-    if(unidad in dict_figuras):
+
+    if(unidad in dict_fig):
         return
-    arch_path = "Cuestionario/Unidad{}/fig{}.txt".format(unidad,unidad)
+
+    dict_fig[unidad] = list()
+    arch_path = "Cuestionario/Unidad{0}/fig{0}.txt".format(unidad)
 
     with open(arch_path,'r') as arch:
-        cont = 1
-        string_fig = ""
+        texto = ""
+        cont = 0
+
         for linea in arch:
+            if linea.startswith("#") or not linea.strip:
+                continue
             if linea.startswith("begin"):
-                string_fig = ""
-            if not linea.startswith("#") or not linea.strip():
-                string_fig += linea.strip()+"\n"
-            if linea.startswith("end"):
-                dict_fig[unidad][cont] = string_fig
+                texto = ""
+            elif linea.startswith("end"):
+                dict_fig[unidad].append(texto)
                 cont += 1
+            else:
+                texto += linea
+
+def leerAlt(unidad,arch_path):
+    list_preguntas = list()
+    with open(arch_path,'r') as arch:
+
+        texto = ""
+        respuesta = ""
+        explicacion = ""
+
+        for linea in arch:
+            if linea.startswith("#") or not linea.strip:
+                continue
+            if linea.startswith("begin"):
+                x,respuesta,explicacion = linea.strip().split("|")
+                if explicacion == "":
+                    explicacion = "No explicacion entregada"
+            elif linea.startswith("figura"):
+                x,fig = linea.strip().split(":")
+                texto += dict_fig[unidad][int(fig)-1]
+            elif linea.startswith("end"):
+                if respuesta != "x" or respuesta != "X":
+                    list_preguntas.append([texto,respuesta,explicacion])
+                texto = ""
+                respuesta = ""
+                explicacion = ""
+            else:
+                texto += linea
+
+    return list_preguntas
 
 def getAlt(unidad):
     if(unidad in dict_alt):
         return
-    arch_path = "Cuestionario/Unidad{}/alt{}.txt".format(unidad,unidad)
+    if(unidad in dict_alt_ref):
+        return
+    
+    getFig(unidad)
+    dict_alt[unidad] = leerAlt(unidad,"Cuestionario/Unidad{0}/alt{0}.txt".format(unidad))
+    dict_alt_ref[unidad] = leerAlt(unidad,"Cuestionario/Unidad{0}/alt{0}ref.txt".format(unidad))
 
+def leerVf(arch_path):
+    list_preguntas = list()
     with open(arch_path,'r') as arch:
-        cont = 1
 
-        str_respuesta = "x"
-        str_texto = ""
-        str_explicacion = ""
-        bool_isEvaluacion = False
-
-        getFig(unidad)
-        
         for linea in arch:
-            if linea.startswith("begin"):
-                str_texto = ""
-                str_respuesta = linea.strip().split("|")[-1]
-            
-            if not linea.startswith("#") or not linea.strip():
+            if linea.startswith("#") or not linea.strip():
+                continue
+            pregunta,respuesta,explicacion = linea.strip().split("|")
+            if respuesta != "x" or respuesta != "X":
+                list_preguntas.append([pregunta,respuesta,explicacion])
 
-                if linea.startswith("figura"):
-                    figuras = linea.strip.split(":")[1].split(",")
-                    for fig in figuras:
-                        str_texto += dict_fig[unidad][fig]
-
-                else:
-                    str_texto += linea.strip()+"\n"
-
-            if linea.startswith("end"):
-                str_explicacion = linea.strip().split("|")[-1]
-                dict_alt[unidad][cont] = [str_texto,str_respuesta,str_explicacion]
-                cont += 1
-                
+    return list_preguntas
 
 def getVf(unidad):
     if(unidad in dict_vf):
         return
-    arch_path = "Cuestionario/Unidad{}/vf{}.txt".format(unidad,unidad)
-
-
-def makeFiguras(unidad):
-    if int(unidad) not in dict_figuras:
-
-        dict_figuras[unidad] = dict()
-        texto_figura = ""
-        cont = 0
-
-        if unidad == 5:
-            nombre_arch = "Cuestionario/Unidad5/figurasU5.txt"
-        
-        with open(nombre_arch,"r") as arch:
-            for linea in arch:
-                if linea.startswith("figura") or not linea.strip():
-                    dict_figuras[unidad][cont] = texto_figura
-                    cont += 1
-                    texto_figura = ""
-                    continue
-                texto_figura = texto_figura+linea[1:]
-
-def getVerdaderoFalso(unidad, cantidad):
-    nombre_arch = ""
-    if unidad == 5:
-        nombre_arch = "Cuestionario/Unidad5/verdaderoFalsoU5.txt"
+    if(unidad in dict_vf_ref):
+        return
     
-    list_preguntas = []
-    with open(nombre_arch,"r") as arch:
-        for linea in arch:
-            if linea.startswith("#") or not linea.strip():
-                continue
-            pregunta, respuesta = linea.strip().split("|")
-            pregunta = "[V-F] "+pregunta
-            if(respuesta != "X"):
-                list_preguntas.append([pregunta.replace("\\n","\n"), respuesta])
-
-    return random.sample(list_preguntas, cantidad)
-
-def getOpcionMultiple(unidad, cantidad):
-    nombre_arch = ""
-    if unidad == 5:
-        nombre_arch = "Cuestionario/Unidad5/opcionMultipleU5.txt"
-
-    list_preguntas = []
-    if(unidad not in dict_figuras):
-        makeFiguras(unidad)
-    pregunta_actual = ["","",""]
-
-    with open(nombre_arch,"r") as arch:
-        for linea in arch:
-            if linea.startswith("#") or not linea.strip():
-                continue
-            if linea.startswith("Pregunta") and pregunta_actual != ["","",""]:
-                list_preguntas.append(pregunta_actual)
-            if linea.startswith("Pregunta"):
-                pregunta_actual = ["[a,b,c,d]","",""]
-                linea = linea.strip().split("|")
-                if linea[1] != " ":
-                    figuras = linea[1].split(",")
-                    for figura in figuras:
-                        pregunta_actual[1] += dict_figuras[unidad][int(figura)]
-                pregunta_actual[2] += linea[2]
-            else:
-                pregunta_actual[0] += linea[1:]
-
-    return random.sample(list_preguntas, cantidad)
-
+    dict_vf[unidad] = leerVf("Cuestionario/Unidad{0}/vf{0}.txt".format(unidad))
+    dict_vf_ref[unidad] = leerVf("Cuestionario/Unidad{0}/vf{0}ref.txt".format(unidad))
+    
 def iniciarQuiz(unidad):
-    correctas_vf = 0
-    correctas_alt = 0
-    makeFiguras(unidad)
-    opcionesVerdaderoFalso = getVerdaderoFalso(unidad,10)
-    for pregunta, respuesta in opcionesVerdaderoFalso:
-        usuario_respuesta = input(pregunta + "\n")
-        if usuario_respuesta.lower() == respuesta.lower():
-            print("Respuesta correcta!")
-            correctas_vf += 1
-        else:
-            print("La respuesta correcta era "+respuesta)
-        input("Pulse enter para continuar:\n")
-        os.system('cls')
 
-    opcionesMultiples = getOpcionMultiple(unidad,5)
-    for pregunta,figuras,respuesta in opcionesMultiples:
-        pregunta = pregunta.replace('\n','\n'+figuras,1)
-        usuario_respuesta = input(pregunta + "\n")
-        if usuario_respuesta.lower() == respuesta.lower():
-            print("Respuesta correcta!")
-            correctas_alt += 1
-        else:
-            print("La respuesta correcta era "+respuesta)
-        input("Pulse enter para continar:\n")
-        os.system('cls')
-    
-    nota = correctas_alt*10 + correctas_vf*5
-    print("TEST TERMINADO: Resultados son:")
-    print("Correctas Verdadero y Falso: "+str(correctas_vf)+"\nCorrectas Opcion Multiple: "+str(correctas_alt)+"\nNota final: "+str(nota))
+    getVf(unidad)
+    getAlt(unidad)
+    getFig(unidad)
 
-def main():
+    list_vf = dict_vf[unidad]
+    list_vf2 = dict_vf_ref[unidad]
+    list_vf.extend(list_vf2)
+
+    list_alt = dict_alt[unidad]
+    list_alt_ref = dict_alt_ref[unidad]
+    list_alt.extend(list_alt_ref)
+
+    preguntasVf = random.sample(list_vf,10)
+    preguntasAlt = random.sample(list_alt,5)
+
+    correcto_vf = 0
+    correcto_alt = 0
 
     os.system('cls')
-    print("Inicio Quiz: Que se desea hacer")
+    for pregunta,respuesta,explicacion in preguntasVf:
+        usuario = input("[V-F] "+pregunta+"\n")
+        if (usuario.lower() == respuesta.lower()):
+            print("Respuesta correcta!")
+            correcto_vf += 1
+        else:
+            print("La respuesta correcta era: "+respuesta+"\n Explicacion: "+explicacion)
+
+        input("Pulsar enter para continuar")
+        os.system('cls')
+
+    for pregunta,respuesta,explicacion in preguntasAlt:
+        usuario = input("[a,b,c,d] "+pregunta+"\n")
+        if (usuario.lower() == respuesta.lower()):
+            print("Respuesta correcta!")
+            correcto_alt += 1
+        else:
+            print("La respuesta correcta era: "+respuesta+"\n Explicacion: "+explicacion)
+
+        input("Pulsar enter para continuar")
+        os.system('cls')
+
+    print("Quiz terminado")
+    print("Verdadero-falso correctas: "+str(correcto_vf))
+    print("Alternativas correctas: "+str(correcto_alt))
+    puntaje = correcto_alt*10 + correcto_vf*5
+    print("Puntaje obtenido: "+str(puntaje))
+    input("Pulsar enter para continuar")
+
+def main():
     decision = 0
     while(decision != 3):
-        decision = input("1. Realizar Quiz (U5) | 2. Realizar Certamen | 3. Salir\n")
-        decision = int(decision)
+        os.system('cls')            
+        print("Bienvenido a Cuestionario.py \n ¿Que desea realizar?")
+        decision = int(input("1. Realizar Quiz (Unidad 5) | 2. Realizar Certamen | 3. Terminar \n"))
+
         if(decision == 1):
-            os.system('cls')
             iniciarQuiz(5)
-        elif(decision == 2):
-            print("No disponible Actualmente")
-            input("Pulse enter para continar:\n")
-            os.system('cls')            
-
-
-
+        
+        if(decision == 2):
+            print("No disponible aun")
 
 main()
-
-
